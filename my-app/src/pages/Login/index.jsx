96c03ed3c3118904c9450940fd85a5b4341d9463
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-08-25 14:07:44 -0300

    Login page

--- my-app/src/pages/Login/index.jsx
+++ my-app/src/pages/Login/index.jsx
@@ -1,25 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory, Link } from "react-router-dom";
+import Axios from 'axios';
 import Logo from '../../assets/logo/logo.svg';
 import Back from '../../assets/images/back_button.svg';
-import './index.css';
+import './styleLogin.css';
+
 
 function Login() {
+  const history = useHistory();
+
+  const url = "http://localhost:8080/login";
+  const [login, setLogin] = useState({
+    email: "",
+    password: ""
+  });
+
+  function handleLoginSubmit(formData) {
+    formData.preventDefault();
+    Axios.post(url, {
+      email: login.email,
+      password: login.password
+    })
+      .then(res => {
+        console.log(res);
+        if (res.status === 200) {
+          history.push(`/${res.data}`);
+          console.log("Connected")
+        }
+      })
+      .catch(() => {
+        document.getElementById('error-message').textContent = "Check your info again";
+        console.log("Error trying to connect")
+      })
+  }
+
+  function handleLoginChange(data) {
+    const newData = { ...login };
+    newData[data.target.id] = data.target.value;
+    setLogin(newData);
+  }
+
   return (
-    <container>
+    <div className="container-login">
+      <Link to="/">
         <img id="logo" src={Logo} alt="Shopetronic icon" />
-        <div className="forms">
-          <h1>Login</h1>
-          <input type="text" className="form-control" placeholder="E-mail" required/>
-          <input type="password" className="form-control" placeholder="Password" required/>
-          <button className="btn-login">Login</button>
-          <span>Sign In here</span>
+      </Link>
+      <div className="forms">
+        <h1>Login</h1>
+        <form id="login-form" onSubmit={(formData) => handleLoginSubmit(formData)}>
+          <input onChange={(data) => handleLoginChange(data)} id="email" value={login.email} type="email" className="form-control" placeholder="E-mail" required />
+          <input onChange={(data) => handleLoginChange(data)} id="password" value={login.password} type="password" minLength="3" className="form-control" placeholder="Password" required />
+          <button className="btn-login" type="submit">Login</button>
+        </form>
+        <span><Link to="/signin" className="signin-button">Sign In here</Link></span>
+        <span id="error-message"></span>
       </div>
-      <div className="back-button">
-      <img id="back-button-img" src={Back} alt="Back button" />
-      <span>Back</span>
+      <div className="back-button" onClick={() => window.history.back()}>
+        <img id="back-button-img" src={Back} alt="Back button" />
+        <span>Back</span>
       </div>
 
-    </container>
+    </div>
   );
 }
 
