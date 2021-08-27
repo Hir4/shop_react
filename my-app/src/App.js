18902240cef7d45551b507299f90527f4d50c1ce
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-08-27 16:44:00 -0300

    Sign In route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login/index.jsx"
+import Signin from "./pages/Signin/index.jsx"
 import Home from "./pages/Home/index.jsx"
 import Product from "./pages/Product/index.jsx"
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
            <Route component = { Home } path="/" exact/>
            <Route component = { Login }  path="/login" exact/>
+           <Route component = { Signin }  path="/signin" exact/>
            <Route component = { Product } path="/product" exact/>
        </BrowserRouter>
     </div>
