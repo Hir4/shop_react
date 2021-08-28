edbdd18f945972374a32421cde59949b4208dcd6
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-08-28 03:04:01 -0300

    Get the product

--- my-app/src/pages/Home/index.jsx
+++ my-app/src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header/index.jsx';
+import HeaderAfterLogin from '../../components/HeaderAfterLogin/index.jsx';
 import GraphicVideo from '../../assets/images/graphic_video.svg';
+import Products from '../../components/ProductsList/index.jsx'
 import './styleHome.css';
 
 function Home() {
+
+  function getHeader(){
+  if(document.cookie){
+    return <HeaderAfterLogin />
+  } else{return <Header />}}
+
   return (
     <React.Fragment>
-      <Header />
+      {getHeader()}
       <div className="container-banner">
         <div id="container-banner-content">
           <h1>Everything you need for your setup.</h1>
@@ -15,38 +23,7 @@ function Home() {
         </div>
         <img src={GraphicVideo} alt="Graphic video" />
       </div>
-      <div className="list-products">
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-        <div className="product">
-          <img src="#" alt="product" />
-        </div>
-      </div>
+     <Products />
     </React.Fragment>
   );
 }
