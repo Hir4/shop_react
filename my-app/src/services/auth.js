069e0504649905668b2ae285a3ea9f74ee4d4b29
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-09-04 11:02:04 -0300

    Removing console.log

--- my-app/src/services/auth.js
+++ my-app/src/services/auth.js
@@ -1,12 +1,7 @@
-// import jwt from 'jsonwebtoken';
-
 const isAllowed = () => {
   if (document.cookie) {
-    // const cookie = document.cookie;
-    // const cookieDecoded = jwt.decode(cookie);
     return true
   } else {
-    console.log("nao entrou")
     return false
   }
 }
